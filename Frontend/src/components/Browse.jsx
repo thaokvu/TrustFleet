0eef6a6558f565ae26e8386d9f3406cead28ac2e
Khiem Nguyen <57775422+KimotoNari@users.noreply.github.com>
2024-11-13 16:59:17 -0600

    Created Search Function
    General search function, still needs calls to backend and creation of the filter component. Does not have any UI elements implemented yet.

--- Frontend/src/components/Browse.jsx
+++ Frontend/src/components/Browse.jsx
@@ -1,4 +1,34 @@
+import { Box, Card, FormControl, FormLabel, Typography, TextField, Button } from "@mui/material"
+import { useState } from "react"
+
 export default function Browse() {
+  const filters = [ //TODO make filter list
+    ""
+  ];
+
+  const vehicleList = [ // this is a temporary list
+    //TODO make actual call to backend for list of vehicles using the filters
+
+  ];
+
+  const [vehicles, setVehicles] = useState(vehicleList);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  //filter fetched vehicles by the search term
+  function handleSearchClick() {
+    //maybe handle the backend request here?
+    if(searchTerm === "") {
+      setVehicles(vehicleList);
+      return;
+    }
+    const filterBySearch = vehicleList.filter((vehicle) => {
+      if(/*vehiclename*/.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return vehicle;
+      }
+      setVehicles(filterBySearch);
+    })
+  }
+
   return (
     <>
     </>
